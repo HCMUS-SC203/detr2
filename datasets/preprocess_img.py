1bc7f833918a893cea5899276fcd8c6682a0cd78
nhphucqt <hoangphuc.qti@gmail.com>
2023-12-12 02:36:50 +0700

    updated

--- datasets/preprocess_img.py
+++ datasets/preprocess_img.py
@@ -9,30 +9,35 @@ padding = 250
 
 def activation(x, size):
     if x >= size - padding:
-        return 0
+        return 0xFF
     # return 1
     size -= padding
     threshold = 2/5
     ratio = x/size - threshold
+    # if ratio < 0:
+    #     return np.exp(ratio*5) / 1.5
+    # return np.tanh((ratio + 1) * 3)
     if ratio < 0:
-        return np.exp(ratio*2) / 1.5
-    return np.tanh((ratio + 1) * 3)
+        return 0xFF
+    return 0
 
 def process_image(img):
     img_array = np.asarray(img)
     vectorized_function = np.vectorize(activation, excluded=['size'])
     filter_vector = vectorized_function(np.arange(img_array.shape[0]), size=img_array.shape[0])
-    # print(filter_vector)
+    print(filter_vector)
     # print(img_array)
     # print(img_array.shape)
-    img_array = np.apply_along_axis(lambda x: np.uint8(np.round(x * filter_vector)), 0, img_array)
+    # img_array = np.apply_along_axis(lambda x: np.uint8(np.round(x * filter_vector)), 0, img_array)
+    img_array = np.apply_along_axis(lambda x: np.uint8(np.bitwise_or(x, filter_vector)), 0, img_array)
     # print(img_array)
     # print(img_array.shape)
     img.close()
     img = Image.fromarray(img_array)
     return img
 
-# img = Image.open("000020.jpg")
-# img = process_image(img)
-# img.show()
-# img.close()
+if __name__ == "__main__":
+    img = Image.open("000029.jpg")
+    img = process_image(img)
+    img.show()
+    img.close()

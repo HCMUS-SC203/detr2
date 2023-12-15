f475143516ed6ce9c00af5d2a57b83981f2739e5
nhphucqt <hoangphuc.qti@gmail.com>
2023-12-15 22:35:28 +0700

    updated

--- datasets/coco.py
+++ datasets/coco.py
@@ -34,7 +34,7 @@ class CocoDetection(torchvision.datasets.CocoDetection):
         # print(type(img))
         # img = pre_img.process_image(img)
         print("Start Filtering...");
-        # img = self.imageFilter(img)
+        img = self.imageFilter(img)
         print("Finish Filtering")
         if self._transforms is not None:
             img, target = self._transforms(img, target)
